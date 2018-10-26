c40bad899ea31f08e517796dcfccfd3e98da54a5
Michael-MO <Michael-MO@users.noreply.github.com>
2018-10-26 11:19:54 +0200

    Setup for MSSQL DB Use

--- HighscoreRest/Controllers/HighscoresController.cs
+++ HighscoreRest/Controllers/HighscoresController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using HighscoreModel;
+using System.Data.SqlClient;
 
 namespace HighscoreRest.Controllers
 {
@@ -13,11 +14,39 @@ namespace HighscoreRest.Controllers
     public class HighscoresController : ControllerBase
     {
         private static List<Highscore> highscoreList = new List<Highscore>();
+        private static string conString;
 
-        // GET: api/Highscores
-        [HttpGet]
+    // GET: api/Highscores
+    [HttpGet]
         public IEnumerable<Highscore> Get()
         {
+            string connectionString = conString;
+            
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                
+                using (SqlCommand command = new SqlCommand("SELECT * FROM HighScore", con))
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Highscore obj = new Highscore();
+                        obj.ID = Convert.ToInt32(reader["ID"]);
+                        obj.Name = reader["Name"].ToString();
+                        obj.Score = Convert.ToInt32(reader["Score"]);
+                        
+                        if (highscoreList.All(I => I.ID != Convert.ToInt32(reader["ID"])))
+                        {
+                            highscoreList.Add(obj);
+                        }
+                    }
+                }
+
+                con.Close();
+            }
+
             return highscoreList;
         }
 
@@ -25,9 +54,24 @@ namespace HighscoreRest.Controllers
         [HttpPost]
         public void Post([FromBody] Highscore obj)
         {
-            obj.ID = highscoreList.Count;
-            highscoreList.Add(obj);
-            highscoreList.OrderByDescending(o => o.Score).ToList();
+            string connectionString = conString;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                
+                using (SqlCommand command =
+                    new SqlCommand("INSERT INTO HighScore(Name, Score) VALUES(@Name, @Score)", con))
+                {
+                    command.Parameters.AddWithValue("@Name", obj.Name);
+                    command.Parameters.AddWithValue("@Score", obj.Score);
+                    command.ExecuteNonQuery();
+                }
+
+                con.Close();
+
+            }
+
         }
 
         // PUT: api/Highscores/5
